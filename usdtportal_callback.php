0120529788ffd6ad3e9d31621c485e6bf892fb69
usdtportal <143783400+usdtportal@users.noreply.github.com>
2023-09-02 20:25:56 +0200

    Update usdtportal_callback.php

--- usdtportal_callback.php
+++ usdtportal_callback.php
@@ -1,7 +1,7 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(1);
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
+error_reporting(0);
 date_default_timezone_set('Europe/Warsaw');
 
 
